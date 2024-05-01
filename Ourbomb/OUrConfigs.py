d392e9827c1cad190c0afbb5dff23871905531c8
Hurtharvey <146387326+Hurtharvey@users.noreply.github.com>
2024-05-01 09:30:01 -0400

    Changed configs

--- Ourbomb/OUrConfigs.py
+++ Ourbomb/OUrConfigs.py
@@ -5,11 +5,11 @@
 #################################
 
 # constants
-DEBUG = True        # debug mode?
+DEBUG = False        # debug mode?
 RPi = True           # is this running on the RPi?
-ANIMATE = False       # animate the LCD text?
-SHOW_BUTTONS = True # show the Pause and Quit buttons on the main LCD GUI?
-COUNTDOWN = 15      # the initial bomb countdown value (seconds)
+ANIMATE = True       # animate the LCD text?
+SHOW_BUTTONS = False # show the Pause and Quit buttons on the main LCD GUI?
+COUNTDOWN = 120      # the initial bomb countdown value (seconds)
 NUM_STRIKES = 5      # the total strikes allowed before the bomb "explodes"
 NUM_PHASES = 4       # the total number of initial active bomb phases
 # the various image and audio files
